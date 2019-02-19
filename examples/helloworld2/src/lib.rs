fbec1b64b90f2fcebc5d25fd76faa8a1c09aea86
lucas7788 <sishsh@163.com>
2019-02-19 16:18:42 +0800

    update helloworld2

--- examples/helloworld2/src/lib.rs
+++ examples/helloworld2/src/lib.rs
@@ -2,7 +2,25 @@
 extern crate ontio_std as ostd;
 use ostd::prelude::*;
 use ostd::abi::{Sink, Source};
-use ostd::runtime;
+use ostd::{database, runtime};
+
+fn hello() -> String {
+    "hello world".to_string()
+}
+
+fn hello2(data: &str) -> String {
+    format!("hello {}", data)
+}
+
+fn save(key:&str, value:&str) -> bool {
+    database::put(key, value);
+    true
+}
+
+fn get(key:&str) -> String {
+    let value:String = database::get(key).unwrap_or_default();
+    value
+}
 
 #[no_mangle]
 pub fn invoke() {
@@ -10,10 +28,23 @@ pub fn invoke() {
     let action = source.read::<String>().unwrap();
     let mut sink = Sink::new(12);
     match action.as_str() {
-        "hello" => sink.write("hello world".to_string()),
+        "hello" => sink.write(hello()),
+        "hello2" => {
+            let data :String= source.read().unwrap_or_default();
+            sink.write(hello2(&data));
+        },
+        "save" => {
+            let key:String = source.read().unwrap_or_default();
+            let value:String = source.read().unwrap_or_default();
+            sink.write(save(&key, &value));
+        },
+        "get" => {
+            let key:String = source.read().unwrap_or_default();
+            sink.write(get(&key));
+        }
         _ => panic!("unsupported action!"),
     }
-
     runtime::ret(&sink.into())
 }
+
 
