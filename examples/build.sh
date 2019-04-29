58aae28981abd5eb33e2d49bd234f9b9f2cbdb9f
lucas7788 <sishsh@163.com>
2019-04-29 10:00:29 +0800

    refactor code

--- examples/build.sh
+++ examples/build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-cargo build --release --target wasm32-unknown-unknown
-cp ./target/wasm32-unknown-unknown/release/demo.wasm  wasm_demo.wasm
-./wasm-prune -e add_one,add,boxed wasm_demo.wasm  wasm_demo_prune.wasm
+#cargo build --release --target wasm32-unknown-unknown
+cp ../target/wasm32-unknown-unknown/release/token.wasm  wasm_demo.wasm
+wasm-prune -e invoke wasm_demo.wasm  wasm_demo_prune.wasm
 wasm2wat wasm_demo.wasm  -o wasm_demo.wast
 wasm2wat wasm_demo_prune.wasm  -o wasm_demo_prune.wast
 wat2wasm wasm_demo_prune.wast  -o wasm_demo_prune_no_custom.wasm
